5d89a8f75390a11f3a6be68e40726b15e125d08f
Skill Template <template@skill>
2026-06-14 16:30:59 +0800

    fix: add /login route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router';
 import DesktopPage from '@/pages/DesktopPage';
+import Login from '@/pages/Login';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<DesktopPage />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<div>404</div>} />
     </Routes>
   );
